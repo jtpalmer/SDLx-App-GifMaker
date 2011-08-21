f8e1fd16b254bacf83fd90b969a365dfe544055f
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-08-21 13:40:38 -0400

    Tiny improvements

--- examples/test.pl
+++ examples/test.pl
@@ -1,24 +1,26 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
-use SDLx::App;
 use SDLx::App::GifMaker;
 
 my $app = SDLx::App::GifMaker->new(
     output_file => '/tmp/test.gif',
     width       => 640,
     height      => 480,
+    dt          => 0.01,
     eoq         => 1,
 );
 
 $app->add_show_handler(
     sub {
         $app->draw_line(
-            [ 0,            0 ],
+            [ rand $app->w, rand $app->h ],
             [ rand $app->w, rand $app->h ],
             [ rand 255, rand 255, rand 255 ]
         );
-        $app->update;
+        $app->update();
     }
 );
 
-$app->run;
+$app->run();
+
